7a629f866b09f450f231a69307bf1f8fe8b3b8fc
Deepika A H <155080797+Deepika743@users.noreply.github.com>
2024-01-30 22:26:40 +0530

    Create print.py

---
+++ print.py
@@ -0,0 +1,7 @@
+#print function
+if __name__ == '__main__':
+    n = int(input().strip())
+    
+    # Loop through numbers from 1 to n (inclusive)
+    for i in range(1, n + 1):
+        print(i, end='')
