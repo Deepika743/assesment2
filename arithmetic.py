af5c2f6d9f40839670e0126fa3d9cf523a225b46
Deepika A H <155080797+Deepika743@users.noreply.github.com>
2024-01-30 22:30:31 +0530

    Create arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,16 @@
+#arithmetic operation
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+if __name__ == '__main__':
+    year = int(input().strip())
+    print(is_leap(year))
